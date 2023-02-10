b53fbf1977a316fa6328ae092e6c8d319011a9cf
Adam Denes <58705091+adamdenes@users.noreply.github.com>
2023-02-10 11:18:43 +0100

    feat: refactoring classes

--- amazing-numbers/src/Main.java
+++ amazing-numbers/src/Main.java
@@ -2,12 +2,13 @@ package numbers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         final Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to Amazing Numbers!\n");
+        System.out.println("Welcome to Amazing Numbers!");
         printHelper();
         MyNumber[] numbers;
         MyNumber n;
@@ -22,25 +23,58 @@ public class Main {
                 continue;
             }
 
-            long num = Long.parseLong(input[0]);
-
-            if (num == 0) {
-                System.out.println("\nGoodbye!");
-                break;
-            }
-
             try {
-                if (input.length < 2) {
-                    n = new MyNumber(num);
-                    System.out.println(n);
-                } else {
-                    long numTwo = Long.parseLong(input[1]);
-                    numbers = new MyNumber[Math.abs((int) numTwo)];
+                MyNumber.validate(input);
 
-                    for (int i = 0; i < numbers.length; i++) {
-                        numbers[i] = new MyNumber(num + i, numTwo);
+                long num = Long.parseLong(input[0]);
+                long numTwo;
+
+                if (num == 0) {
+                    System.out.println("\nGoodbye!");
+                    break;
+                }
+
+                switch (input.length) {
+                    case 1 -> {
+                        n = new MyNumber(num);
+                        System.out.println(n);
                     }
-                    process(numbers);
+                    case 2 -> {
+                        numTwo = Long.parseLong(input[1]);
+                        numbers = new MyNumber[Math.abs((int) numTwo)];
+
+                        for (int i = 0; i < numbers.length; i++) {
+                            numbers[i] = new MyNumber(num + i, numTwo);
+                        }
+
+                        processProperties(numbers);
+                    }
+                    default -> {
+                        numTwo = Long.parseLong(input[1]);
+                        String property = input[2];
+
+                        if (!isProperty(property)) {
+                            printHelper(property);
+                            continue;
+                        }
+
+                        n = new MyNumber(num);
+                        int propCount = 0;
+                        System.out.println();
+                        while (propCount < numTwo) {
+                            List<String> stringList = gatherProperties(n);
+                            String s = String.join(", ", stringList);
+
+                            for (String sl : stringList) {
+                                if (Objects.equals(sl.toUpperCase(), property)) {
+                                    System.out.println("\t\t\t   " + n.getNum() + " is " + s);
+                                    propCount++;
+                                }
+                            }
+                            n.setNum(n.getNum() + 1);
+                        }
+                        System.out.println();
+                    }
                 }
             } catch (MyNeturalNumberException e) {
                 System.out.println(e.getMessage());
@@ -48,22 +82,44 @@ public class Main {
         } while (true);
     }
 
-    public static void process(MyNumber[] numbers) {
+    public static void processProperties(MyNumber[] numbers) {
         System.out.println();
         for (MyNumber number : numbers) {
             String s = String.join(", ", gatherProperties(number));
-            System.out.println(number.getNum() + " is " + s);
+            System.out.println("\t\t\t   " + number.getNum() + " is " + s);
         }
         System.out.println();
     }
 
+    public static boolean isProperty(String property) {
+        String[] properties = {"EVEN", "ODD", "BUZZ", "DUCK", "PALINDROMIC", "GAPFUL", "SPY", "SQUARE", "SUNNY"};
+        for (String p : properties) {
+            if (Objects.equals(p, property.toUpperCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void printHelper(String property) {
+        String help = String.format("""
+                                
+                The property [%s] is wrong.
+                Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY]
+                """, property.toUpperCase());
+        System.out.println(help);
+    }
+
     public static void printHelper() {
-        String help = """                 
+        String help = """ 
+                                
                 Supported requests:
                 - enter a natural number to know its properties;
                 - enter two natural numbers to obtain the properties of the list:
                   * the first parameter represents a starting number;
                   * the second parameter shows how many consecutive numbers are to be printed;
+                - two natural numbers and a property to search for;
+                - two natural numbers and two properties to search for;
                 - separate the parameters with one space;
                 - enter 0 to exit.
                 """;
@@ -85,6 +141,15 @@ public class Main {
         if (num.isGapful()) {
             properties.add("gapful");
         }
+        if (num.isSpy()) {
+            properties.add("spy");
+        }
+        if (num.isSquare()) {
+            properties.add("square");
+        }
+        if (num.isSunny()) {
+            properties.add("sunny");
+        }
         if (num.isEven()) {
             properties.add("even");
         }
@@ -93,79 +158,5 @@ public class Main {
         }
 
         return properties;
-    }
-}
-
-class MyNumber {
-    private final long num;
-
-    public MyNumber(long num) throws MyNeturalNumberException {
-        if (num < 0) {
-            throw new MyNeturalNumberException("The first parameter should be a natural number or zero.\n");
-        }
-        this.num = num;
-    }
-
-    public MyNumber(long num1, long num2) throws MyNeturalNumberException {
-        this(num1);
-        if (num2 < 0) {
-            throw new MyNeturalNumberException("The second parameter should be a natural number.\n");
-        }
-    }
-
-    public long getNum() {
-        return num;
-    }
-
-    protected boolean isEven() {
-        return this.num % 2 == 0;
-    }
-
-    protected boolean isOdd() {
-        return !this.isEven();
-    }
-
-    protected boolean isBuzz() {
-        return this.num % 7 == 0 || this.num % 10 == 7;
-    }
-
-    protected boolean isDuck() {
-        return String.valueOf(this.num).contains("0") && String.valueOf(this.num).indexOf("0") != 0;
-    }
-
-    protected boolean isGapful() {
-        String fakeNum = String.valueOf(this.num);
-        if (fakeNum.length() >= 3) {
-            String firstDigit = fakeNum.substring(0, 1);
-            String lastDigit = fakeNum.substring(fakeNum.length() - 1);
-            long divisor = Long.parseLong(firstDigit.concat(lastDigit));
-
-            return this.num % divisor == 0;
-        }
-        return false;
-    }
-
-    protected boolean isPalindromic() {
-        String number = String.valueOf(this.num);
-        for (int i = 0; i < number.length(); i++) {
-            int last = number.length() - 1;
-            if (number.charAt(i) == number.charAt(last - i)) {
-                continue;
-            }
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("\nProperties of " + this.num +
-                "\n\t\tbuzz: " + this.isBuzz() +
-                "\n\t\tduck: " + this.isDuck()) +
-                "\n palindromic: " + this.isPalindromic() +
-                "\n\t  gapful: " + this.isGapful() +
-                "\n\t\teven: " + this.isEven() +
-                "\n\t\t odd: " + this.isOdd() +
-                "\n";
     }
 }
