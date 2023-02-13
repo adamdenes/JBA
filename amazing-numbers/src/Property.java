ca8a27024b41c50534514bcc1c07d3dabc2e3c1d
Adam Denes <58705091+adamdenes@users.noreply.github.com>
2023-02-13 17:51:39 +0100

    feat: finishing project stage 8 (happy/sad numbers)

--- amazing-numbers/src/Property.java
+++ amazing-numbers/src/Property.java
@@ -12,7 +12,9 @@ public enum Property {
     SPY("spy"),
     SQUARE("square"),
     SUNNY("sunny"),
-    JUMPING("jumping");
+    JUMPING("jumping"),
+    HAPPY("happy"),
+    SAD("sad");
 
     final String type;
 
@@ -21,63 +23,135 @@ public enum Property {
     }
 
     public static boolean isProperty(String property) {
+        String tempProperty = property;
+        if (property.startsWith("-")) {
+            tempProperty = property.replace("-", "");
+        }
+
         for (Property p : values()) {
-            if (property == null) {
-                continue;
-            }
-            if (Objects.equals(p.name(), property.toUpperCase())) {
+            if (Objects.equals(p.name(), tempProperty.toUpperCase())) {
                 return true;
             }
         }
         return false;
     }
 
+    static boolean isFilter(String property) {
+        return property.startsWith("-");
+    }
+
+    private static void processFilters(boolean isCurrentFilter, boolean isNextFilter, String tempProp, String next, String[] result) {
+        if (isCurrentFilter && isNextFilter) {
+            tempProp = "-" + tempProp.toUpperCase();
+            next = "-" + next;
+        } else if (isCurrentFilter) {
+            tempProp = "-" + tempProp.toUpperCase();
+        } else if (isNextFilter) {
+            next = "-" + next;
+        }
+
+        result[0] = tempProp.toUpperCase();
+        result[1] = next;
+    }
+
     public static String[] getMutuallyExclusive(String... properties) {
         String[] result = new String[2];
+        String tempProp;
+        String tempProp2;
         Property next;
 
+
         for (String s : properties) {
+            boolean isCurrentFilter = false;
+            boolean isNextFilter = false;
+            tempProp = s;
             for (String property : properties) {
-                if (property.equals(s)) {
+                tempProp2 = property;
+                if (s.equals(property)) {
                     continue;
                 }
 
-                next = Property.valueOf(property.toUpperCase());
-                switch (Property.valueOf(s.toUpperCase())) {
+                if (isFilter(tempProp)) {
+                    tempProp = s.replace("-", "");
+                    isCurrentFilter = true;
+                }
+                if (isFilter(tempProp2)) {
+                    tempProp2 = property.replace("-", "");
+                    isNextFilter = true;
+                }
+
+                next = Property.valueOf(tempProp2.toUpperCase());
+
+                if (tempProp.equals(tempProp2)) {
+                    processFilters(isCurrentFilter, isNextFilter, tempProp, next.name(), result);
+                    return result;
+                }
+
+                switch (Property.valueOf(tempProp.toUpperCase())) {
                     case EVEN -> {
                         if (next == ODD) {
-                            result[0] = s.toUpperCase();
-                            result[1] = next.name();
+                            processFilters(isCurrentFilter, isNextFilter, tempProp, next.name(), result);
+                            return result;
                         }
                     }
                     case ODD -> {
                         if (next == EVEN) {
-                            result[0] = s.toUpperCase();
-                            result[1] = next.name();
+                            processFilters(isCurrentFilter, isNextFilter, tempProp, next.name(), result);
+                            return result;
                         }
                     }
                     case DUCK -> {
+                        if (property.equals("-spy") || isCurrentFilter) {
+                            continue;
+                        }
                         if (next == SPY) {
-                            result[0] = s.toUpperCase();
-                            result[1] = next.name();
+                            processFilters(false, isNextFilter, tempProp, next.name(), result);
+                            return result;
                         }
                     }
                     case SPY -> {
+                        if (property.equals("-duck") || isCurrentFilter) {
+                            continue;
+                        }
                         if (next == DUCK) {
-                            result[0] = s.toUpperCase();
-                            result[1] = next.name();
+                            processFilters(false, isNextFilter, tempProp, next.name(), result);
+                            return result;
                         }
                     }
                     case SQUARE -> {
+                        if (property.equals("-sunny") || isCurrentFilter) {
+                            continue;
+                        }
                         if (next == SUNNY) {
-                            result[0] = s.toUpperCase();
-                            result[1] = next.name();
+                            processFilters(false, isNextFilter, tempProp, next.name(), result);
+                            return result;
                         }
                     }
                     case SUNNY -> {
+                        if (property.equals("-square") || isCurrentFilter) {
+                            continue;
+                        }
                         if (next == SQUARE) {
-                            result[0] = s.toUpperCase();
-                            result[1] = next.name();
+                            processFilters(false, isNextFilter, tempProp, next.name(), result);
+                            return result;
+                        }
+                    }
+                    case HAPPY -> {
+                        if (next == SAD) {
+                            processFilters(isCurrentFilter, isNextFilter, tempProp, next.name(), result);
+                            return result;
+                        }
+                    }
+                    case SAD -> {
+                        if (next == HAPPY) {
+                            processFilters(isCurrentFilter, isNextFilter, tempProp, next.name(), result);
+                            return result;
+                        }
+                    }
+                    default -> {
+                        if (s.equals(tempProp2) || tempProp.equals(property)) {
+                            processFilters(true, true, tempProp, next.name(), result);
+                            return result;
                         }
                     }
                 }
