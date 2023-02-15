d264adcb50a005a75eb44d52f0176e1a4f620a9e
adamdenes <adam.denes@icloud.com>
2023-02-15 14:26:36 +0100

    feat: finished stage 5 (added more options)

--- rock-paper-scissors/src/Main.java
+++ rock-paper-scissors/src/Main.java
@@ -1,54 +1,160 @@
 package rockpaperscissors;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        File file = new File("rating.txt");
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        Player player = new Player(sc.nextLine());
+        System.out.printf("Hello, %s\n", player.getName());
+
+        String[] options = sc.nextLine().split(",");
+        System.out.println("Okay, let's start");
 
         do {
             String input = sc.nextLine();
             switch (input) {
-                case "rock", "paper", "scissors" -> play(input);
+                case "!rating" -> {
+                    try {
+                        int scoreInFile = getRating(player.getName(), file);
+                        System.out.printf("Your rating: %d\n", player.getScore() + scoreInFile);
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
                 case "!exit" -> {
                     System.out.println("Bye!");
                     return;
                 }
-                default -> System.out.println("Invalid input");
+                default -> play(input, options, player);
             }
         } while (true);
     }
 
-    public static void play(String input) {
-        String[] options = {"rock", "paper", "scissors"};
-        Random random = new Random();
-        int r = random.nextInt(3);
-        evaluate(input, options[r]);
+    public static int getRating(String name, File file) throws FileNotFoundException {
+        Scanner sc = new Scanner(file);
+        System.out.println(file.getPath());
+        while (sc.hasNextLine()) {
+            String[] line = sc.nextLine().split(" ");
+            String player = line[0];
+            int score = Integer.parseInt(line[1]);
+
+            if (name.equals(player)) {
+                return score;
+            }
+        }
+        return 0;
     }
 
-    public static void evaluate(String input, String option) {
+    public static boolean isValidInput(String input, String[] options) {
+        for (var option : options) {
+            if (option.equals(input)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void play(String input, String[] options, Player player) {
+        String[] defaultOptions;
+        Random random = new Random();
+
+        if (options[0].equals("")) {
+            defaultOptions = new String[]{"rock", "paper", "scissors"};
+            if (isValidInput(input, defaultOptions)) {
+                int r = random.nextInt(defaultOptions.length);
+                evaluate(input, defaultOptions[r], player);
+            } else {
+                System.out.println("Invalid input");
+            }
+        } else {
+            if (isValidInput(input, options)) {
+                int r = random.nextInt(options.length);
+                evaluatePlus(input, options[r], options, player);
+            } else {
+                System.out.println("Invalid input");
+            }
+        }
+    }
+
+    public static void evaluate(String input, String option, Player player) {
+        int score = player.getScore();
         if (input.equals(option)) {
+            player.setScore(score + 50);
             draw(option);
         } else if ("paper".equals(input)) {
             if ("scissors".equals(option)) {
                 loss(option);
             } else {
+                player.setScore(score + 100);
                 win(option);
             }
         } else if ("scissors".equals(input)) {
             if ("rock".equals(option)) {
                 loss(option);
             } else {
+                player.setScore(score + 100);
                 win(option);
             }
         } else {
             if ("paper".equals(option)) {
                 loss(option);
             } else {
+                player.setScore(score + 100);
                 win(option);
             }
         }
+    }
+
+    public static void evaluatePlus(String input, String option, String[] options, Player player) {
+        int score = player.getScore();
+        String[] stronger = getStronger(input, options);
+
+        // draw
+        if (input.equals(option)) {
+            player.setScore(score + 50);
+            draw(option);
+            return;
+        }
+
+        // if not stronger
+        for (String s : stronger) {
+            if (Objects.equals(s, option)) {
+                loss(option);
+                return;
+            }
+        }
+
+        // then it is weaker
+        player.setScore(score + 100);
+        win(option);
+    }
+
+    private static String[] getStronger(String option, String[] options) {
+        int half = options.length / 2;
+        String[] result = new String[half];
+
+        int idx = 0;
+        for (int i = 0; i < options.length; i++) {
+            if (option.equals(options[i])) {
+                idx = i;
+            }
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            int index = (idx + i + 1) % options.length;
+            result[i] = options[index];
+        }
+
+        return result;
     }
 
     private static void loss(String option) {
---
+++ rock-paper-scissors/src/Player.java
@@ -0,0 +1,23 @@
+package rockpaperscissors;
+
+public class Player {
+    String name;
+    int score;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
+
